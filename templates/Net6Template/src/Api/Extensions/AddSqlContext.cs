cd44df7f711882c69f2d6b5162a21fc305ba4db3
Roy W Bradley <roy.bradley@opm.gov>
2023-05-25 10:44:20 -0400

    Update to version 1.8.0 bug Fixes

--- templates/Net6Template/src/Api/Extensions/AddSqlContext.cs
+++ templates/Net6Template/src/Api/Extensions/AddSqlContext.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 using Persistence.AppContext;
 
@@ -9,8 +9,10 @@ namespace Api.Extensions;
 public static class AddSqlContext
 {
 	public static void EnableSqlContext(this IServiceCollection services, string connection) =>
-		services.AddDbContext<ApiDbContext>(options => {
-			_ = options.UseSqlServer(connection, o => o.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery));
-			_ = options.EnableDetailedErrors();
+		_ = services.AddDbContext<ApiDbContext>(options => {
+			_ = options.UseSqlServer(connection, builder => {
+				builder.EnableRetryOnFailure(3, TimeSpan.FromSeconds(30), null);
+			});
+			_ = options.EnableDetailedErrors()
 		});
 }
